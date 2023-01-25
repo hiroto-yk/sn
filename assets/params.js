7cf35c91149f0bcba52c1338f48f2356f3991f9f
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2023-01-25 20:29:21 +0900

    first commit

---
+++ assets/params.js
@@ -0,0 +1,66 @@
+export const params = {
+  file: {
+    display: 'File',
+    type: 'file',
+    target: 'rs',
+    default: [],
+    size: 'whole'
+  },
+  form: {
+    display: 'Format',
+    type: 'select',
+    items: [
+      {
+        name: 'hs',
+        display: 'SOL (.hs)',
+      },
+      {
+        name: 'trs',
+        display: 'TRS',
+      },
+      {
+        name: 'haskell',
+        display: 'GHC Rules'
+      }
+    ],
+    default: {
+      name: 'hs',
+      display: 'SOL (.hs)',
+    },
+    size: 'half'
+  },
+  pro: {
+    display: 'Check',
+    type: 'select',
+    items: [
+      {
+        name: 'cr',
+        display: 'CR',
+      },
+      {
+        name: 'cr_CBV',
+        display: 'CR(CBV)',
+      },
+      {
+        name: 'snG',
+        display: 'SN'
+      },
+      {
+        name: 'cri',
+        display: 'WCR'
+      },
+    ],
+    default: {
+      name: 'cr',
+      display: 'CR',
+    },
+    size: 'half'
+  },
+  rs: {
+    display: 'File body',
+    type: 'textarea',
+    rows: 18,
+    default: '',
+    size: 'whole'
+  }
+}
